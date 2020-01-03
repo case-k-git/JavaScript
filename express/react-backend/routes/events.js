15927163264afe2fca2d672e69e57a9125433efd
keisuke-taniguchi <keisuke-taniguchi@dac.co.jp>
2020-01-03 23:15:13 +0900

    APIディレクトリ構成を変更

--- express/react-backend/routes/events.js
+++ express/react-backend/routes/events.js
@@ -4,7 +4,7 @@ const { EventsHandler } = require('../api/handlers/events');
 
 /* GET event listing. */
 // APIの中身は外部に実装しここでreturnする
-router.get('/', function(req, res, next) {
+router.get('/getEventsData', function(req, res, next) {
   return new EventsHandler().getEventsData(req, res);
 });
 module.exports = router;
